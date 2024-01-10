5c02c44339c20f98ff63440b69b39f61e9c8d8a5
MioYss <arnaudhung@gmail.com>
2024-01-10 15:37:14 +0100

    A faire
    ajout data ennemy
    nombre tuer
    
    Faire condition pour unlock gatcha perso
    
    UI pop on bouton

---
+++ Projet_Idle_TU/Assets/Script/Afaire.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Afaire : MonoBehaviour
+{
+   /* ajout data ennemy
+    nombre tuer
+
+    Faire condition pour unlock gatcha perso
+
+    UI pop on bouton
+   */
+}
