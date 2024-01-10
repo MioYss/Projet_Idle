8f1426f2f31766f7af998f4b5756a39b10d4f991
MioYss <arnaudhung@gmail.com>
2024-01-10 15:32:08 +0100

    2 listes

--- Projet_Idle_TU/Assets/Script/Gatcha_Manager.cs
+++ Projet_Idle_TU/Assets/Script/Gatcha_Manager.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class Gatcha_Manager : MonoBehaviour
 {
+    public WeightedList<Data_Gatcha> unlock_gatcha;
+
     public WeightedList<Data_Gatcha> drop_gatcha;
 
     // Start is called before the first frame update
-    public void Debloquage_Liste()
+    public void Unlock_Liste()
     {
+        unlock_gatcha.GetRandomElement();
+    }
+
+    public void Drop_Liste()
+    {
+
         drop_gatcha.GetRandomElement();
+
     }
 
 }
